3b3c2e01927154615684640b8c28c5d6e4075e78
im7mortal <5336231+im7mortal@users.noreply.github.com>
2024-02-14 20:43:30 -0600

    Add test to lib

---
+++ pkg/rpaDecoder/rpaDecoder_test.go
@@ -0,0 +1,40 @@
+package rpaDecoder_test
+
+import (
+	"context"
+	"github.com/golang/glog"
+	"github.com/im7mortal/unrpa/pkg/rpaDecoder"
+	"testing"
+)
+
+const (
+	archivePath = "/home/user/QubesIncoming/basura/archive1.rpa"
+	extractPath = "~/out_rpa"
+)
+
+func TestExtracting(t *testing.T) {
+
+	dcdr, err := rpaDecoder.DetectVersion(archivePath, extractPath)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	ctx := context.TODO()
+
+	list, err := dcdr.List(ctx)
+	if err != nil {
+		glog.Error(err)
+		return
+	}
+
+	for i := range list {
+		glog.Info(list[i].Name)
+	}
+
+	err = dcdr.Decode(ctx)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+}
