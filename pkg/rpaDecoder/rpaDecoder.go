2d80a120cfbd2eb35ead994dd3786786a94cf9d6
im7mortal <5336231+im7mortal@users.noreply.github.com>
2024-02-14 20:43:35 -0600

    Separate utils functions

--- pkg/rpaDecoder/rpaDecoder.go
+++ pkg/rpaDecoder/rpaDecoder.go
@@ -3,11 +3,8 @@ package rpaDecoder
 import (
 	"bufio"
 	"context"
-	"encoding/binary"
-	"encoding/hex"
 	"errors"
 	"github.com/golang/glog"
-	"math/big"
 	"os"
 	"path/filepath"
 	"strings"
@@ -43,22 +40,6 @@ func (ni *notImplemented) Decode(context.Context) error {
 
 func (ni *notImplemented) List(context.Context) ([]FileHeader, error) {
 	return nil, errors.New(ni.v + " is not implemented")
-}
-
-func getInt64(i interface{}) (v int64, err error) {
-	vv, ok := i.(*big.Int)
-	if !ok {
-		if v, ok = i.(int64); ok {
-			return
-		}
-		return 0, errors.New("expected *big.Int")
-	}
-	if !vv.IsInt64() {
-		if !ok {
-			return 0, errors.New("expected int64")
-		}
-	}
-	return vv.Int64(), nil
 }
 
 func DetectVersion(archivePath, extractPath string) (d Decoder, err error) {
@@ -114,20 +95,4 @@ func DetectVersion(archivePath, extractPath string) (d Decoder, err error) {
 		return
 	}
 	return &v3d, err
-}
-
-func stringToInt64(s string) (i int64, err error) {
-	var offset []byte
-	offset, err = hex.DecodeString(s)
-	if err != nil {
-		glog.Error(err)
-		return
-	}
-	switch len(offset) {
-	case 8:
-		return int64(binary.BigEndian.Uint64(offset)), err
-	case 4:
-		return int64(binary.BigEndian.Uint32(offset)), err
-	}
-	return 0, errors.New("wrong size of hex string")
 }
---
+++ pkg/rpaDecoder/utils.go
@@ -0,0 +1,41 @@
+package rpaDecoder
+
+import (
+	"encoding/binary"
+	"encoding/hex"
+	"errors"
+	"github.com/golang/glog"
+	"math/big"
+)
+
+func getInt64(i interface{}) (v int64, err error) {
+	vv, ok := i.(*big.Int)
+	if !ok {
+		if v, ok = i.(int64); ok {
+			return
+		}
+		return 0, errors.New("expected *big.Int")
+	}
+	if !vv.IsInt64() {
+		if !ok {
+			return 0, errors.New("expected int64")
+		}
+	}
+	return vv.Int64(), nil
+}
+
+func stringToInt64(s string) (i int64, err error) {
+	var offset []byte
+	offset, err = hex.DecodeString(s)
+	if err != nil {
+		glog.Error(err)
+		return
+	}
+	switch len(offset) {
+	case 8:
+		return int64(binary.BigEndian.Uint64(offset)), err
+	case 4:
+		return int64(binary.BigEndian.Uint32(offset)), err
+	}
+	return 0, errors.New("wrong size of hex string")
+}
