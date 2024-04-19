59001df317a4b6e856cbb032d0044df9fba441ea
im7mortal <5336231+im7mortal@users.noreply.github.com>
2024-04-19 16:58:42 -0400

    Working React module for chrome

--- unrpapp/src/MyButtonsComponent.tsx
+++ unrpapp/src/MyButtonsComponent.tsx
@@ -1,36 +1,48 @@
-import React, { useState } from 'react';
+import React, {useState, useEffect, useRef} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import * as bowser from "bowser";
-import {FClassInterface, FileApi, FileSystemAccessApi} from './detectVersion';
+import {
+    FClassInterface,
+    FileApi,
+    FileSystemAccessApi,
+    MetadataResponse,
+    FileSystemAccessApiInterface
+} from './detectVersion';
 
 function MyButtonsComponent() {
     // Implement your functions
     const [isFilePicked, setFilePicked] = useState(false);
     const [isDirectoryPicked, setDirectoryPicked] = useState(false);
+    const [isExtracted, setExtracted] = useState(false);
 
     const browser = bowser.getParser(window.navigator.userAgent);
 
-    let fClass: FClassInterface;
-
-
-    let b = browser.getBrowserName();
-    if (b === "Safari" || b === "Firefox") {
-        fClass = new FileApi((s: string) => {
-        }, () => {
-        }, () => {
+    const fClass = useRef<FileSystemAccessApiInterface | null>(null);
+    useEffect(() => {
+        const browser = bowser.getParser(window.navigator.userAgent);
+        let b = browser.getBrowserName();
+        if (b === "Safari" || b === "Firefox") {
+            // fClass.current = new FileApi((s: string) => { });
+        } else {
+            ;
+        }
+        fClass.current = new FileSystemAccessApi((s: string) => {
         })
-    } else {
-        fClass = new FileSystemAccessApi((s: string) => {
-        }, () => {
-        }, () => {
-        });
-    }
+    }, []);
+
 
     const chooseFile = async () => {
         try {
             let [fileHandle] = await window.showOpenFilePicker();
             const file = await fileHandle.getFile();
-            await fClass.extractMetadata(file);
-            setFilePicked(true);
+            let resp: MetadataResponse = await fClass.current!.extractMetadata(file);
+            if (resp.Error === "") {
+                setFilePicked(true);
+            } else {
+                console.log(resp.Error);
+            }
+
         } catch (err) {
             if (err instanceof DOMException && err.name === 'AbortError') {
 
@@ -42,9 +54,10 @@ function MyButtonsComponent() {
     };
 
     const chooseDirectory = async () => {
-        if(window.showDirectoryPicker) {
+        if (window.showDirectoryPicker) {
             try {
                 let directoryHandle = await window.showDirectoryPicker();
+                fClass.current?.setDirectoryHandle(directoryHandle);
                 setDirectoryPicked(true);
             } catch (err) {
                 if (err instanceof DOMException && err.name === 'AbortError') {
@@ -59,16 +72,22 @@ function MyButtonsComponent() {
         }
     };
 
+    const start = async () => {
+        await fClass.current?.extract()
+        setExtracted(true)
+    }
+
 
     const scanDirectory = () => alert('To directory clicked');
 
     return (
         <div className="col">
-            <button id="filePick" className="button-blue" onClick={chooseFile}>Select archive</button>
-            <button id="dirrPick" onClick={chooseDirectory} disabled={!isFilePicked}>To directory</button>
-            <button id="start" onClick={chooseFile} disabled={!isFilePicked}>Extract</button>
-            <span className={`fs-2 font-weight-bold me-3 ms-3 ${isFilePicked ? 'invisible' : ''}`}>OR</span>
-            <button id="selectDirectory" onClick={chooseDirectory}>Scan directory</button>
+
+            <button id="filePick" className={`btn ${isFilePicked ? 'btn-success' : 'btn-primary'} me-3`} onClick={chooseFile}>Select archive</button>
+            <button id="dirrPick" className={`btn ${isFilePicked ? (isDirectoryPicked ? 'btn-success' : 'btn-primary') : 'btn-secondary'} me-3`} onClick={chooseDirectory} disabled={!isFilePicked}>To directory</button>
+            <button id="startExt" className={`btn ${isDirectoryPicked ? (isExtracted ? 'btn-success' : 'btn-primary') : 'btn-secondary'} `} onClick={start} disabled={!isDirectoryPicked}>Extract</button>
+            {/*<span className={`fs-2 font-weight-bold me-3 ms-3 ${isFilePicked ? 'invisible' : ''}`}>OR</span>*/}
+            {/*<button id="selectDirectory" className="btn btn-primary" onClick={chooseDirectory}>Scan directory</button>*/}
         </div>
     );
 
