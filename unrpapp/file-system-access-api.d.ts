8347ed17130c91c5d7501d3a066f8cfd6078681d
im7mortal <5336231+im7mortal@users.noreply.github.com>
2024-04-20 20:35:59 -0400

    Before changing d.ts

--- unrpapp/file-system-access-api.d.ts
+++ unrpapp/file-system-access-api.d.ts
@@ -3,10 +3,20 @@ interface FileSystemHandle {
     name: string;
     queryPermission(opts: any): Promise<PermissionState>;
     requestPermission(opts: any): Promise<PermissionState>;
+    getDirectoryHandle?(name: string, options?: { create: boolean }): Promise<FileSystemDirectoryHandle>;
 }
 
 interface FileSystemFileHandle extends FileSystemHandle {
     getFile(): Promise<File>;
+}
+
+interface FileSystemDirectoryHandle extends FileSystemHandle {
+    values: () => AsyncIterable<FileSystemHandleEntry>;
+}
+
+interface FileSystemHandleEntry {
+    kind: string;
+    name: string;
 }
 
 interface FilePickerOptions {
@@ -17,8 +27,13 @@ interface FilePickerOptions {
 
 interface Window {
     showOpenFilePicker(opts?: FilePickerOptions) : Promise<Array<FileSystemFileHandle>>;
+    showDirectoryPicker?(opts?: {}): Promise<FileSystemDirectoryHandle>;
 }
 
 interface HTMLElement {
     showOpenFilePicker(opts?: FilePickerOptions) : Promise<Array<FileSystemFileHandle>>;
+    showDirectoryPicker?(opts?: {}): Promise<FileSystemDirectoryHandle>;
 }
+
+
+// export { FileSystemHandle, FileSystemFileHandle, FileSystemDirectoryHandle, FileSystemHandleEntry, FilePickerOptions, SomeClass };
