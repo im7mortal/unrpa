618fb846472c35b7bb375abc7c75a0aff0e903cc
im7mortal <5336231+im7mortal@users.noreply.github.com>
2024-05-25 21:12:11 -0400

    implement logic of separation logs by level; regression : doesn't print to the window

--- unrpapp/src/LogProvider.tsx
+++ unrpapp/src/LogProvider.tsx
@@ -1,14 +1,17 @@
 import React, { useReducer, createContext, Dispatch } from 'react';
+import { logLevelFunction, Logger, LogLevel } from './logInterface';
 
 type LogAction = {
   type: 'ADD_LOG';
   payload: string;
 };
 
+
 type LogContextProps = {
   logs: string[];
-  recordLog: (msg: string) => void;
+  recordLog: logLevelFunction;
 };
+
 interface LogProviderProps {
   children: React.ReactNode;
 }
@@ -23,11 +26,32 @@ const logReducer = (state: string[], action: LogAction) => {
   }
 };
 
-export const LogProvider: React.FC<LogProviderProps> = ({ children }) => {
-  const [logs, dispatch] = useReducer(logReducer, []);
+interface LogProviderProps {
+  children: React.ReactNode,
+  loggers: Logger[]
+}
 
-  const recordLog = (message: string) => {
-    dispatch({ type: 'ADD_LOG', payload: message });
+// Declare logProvider function as of type (message: string) => void
+export const DefaultLoggerFunc: (message: string) => void = function logProvider(message: string) {
+  console.log("THIS FUNCTIONS MUST NOT BE CALLED")
+};
+
+export const LogProvider: React.FC<LogProviderProps> = ({ children, loggers }) => {
+  const [logs, dispatch] = useReducer(logReducer, []);
+  console.log("GET HERE GOOD")
+  // Iterate over loggers and replace logFunction of the one that matches fromThisPackage
+  for(let i = 0; i < loggers.length; i++) {
+    if(loggers[i].logFunction === DefaultLoggerFunc) {
+      loggers[i].logFunction = (message: string) => {
+        console.log("GET IN GOOD")
+        dispatch({ type: 'ADD_LOG', payload: message });
+      };
+    }
+  }
+
+  const recordLog = (message: string, logLevel: LogLevel) => {
+    loggers.filter(logger => logger.logLevel >= logLevel)
+        .forEach(logger => logger.logFunction(message));
   };
 
   return (
