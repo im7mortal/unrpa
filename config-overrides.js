772607692c1c82d0b9be3b0b6eb81b856ed4479a
im7mortal <5336231+im7mortal@users.noreply.github.com>
2024-06-03 11:16:10 -0400

    Dev: changes to config-overrides.js

--- config-overrides.js
+++ config-overrides.js
@@ -1,14 +1,25 @@
 const path = require('path');
-const rewireWorkers = require('react-app-rewire-workers');
+
 module.exports = function override(config, env) {
     console.log(config);
-    config.resolve.alias = {...config.resolve.alias, 'worker_threads': false, 'child_process': false,};
-    config.resolve.fallback = {...config.resolve.fallback, 'os': require.resolve('os-browserify/browser'),};
-    config = rewireWorkers(config, env);
+
+    config.resolve.alias = {
+        ...config.resolve.alias,
+        'worker_threads': false,
+        'child_process': false
+    };
+
+    config.resolve.fallback = {
+        ...config.resolve.fallback,
+        'os': require.resolve('os-browserify/browser'),
+    };
+
+    // Add a rule for *.worker.ts files
     config.module.rules.push({
         test: /\.worker\.ts$/,
         include: path.resolve(__dirname, 'src/workers'),
-        use: {loader: 'worker-loader'},
+        use: { loader: 'worker-loader' }
     });
+
     return config;
 };
