97eb0272d1ae04682137399040b5bf196a4797c2
im7mortal <5336231+im7mortal@users.noreply.github.com>
2024-06-05 12:28:21 -0400

    Dev: Refactor workers and core library

--- src/detectVersion.ts
+++ src/detectVersion.ts
@@ -26,9 +26,9 @@ class WorkerPool {
         return WorkerPool.instance;
     }
 
-    public async addTask(data: Uint8Array, keyNumber: number): Promise<any> {
+    public async addTask(file: File): Promise<any> {
         try {
-            return await this.pool.exec('addTask', [data, keyNumber]);
+            return await this.pool.exec('extractMetadata', [file]);
         } catch (error) {
             console.error('Error executing task:', error);
         }
@@ -88,147 +88,14 @@ function newMetadataResponse(FileHeaders: FileHeader[], err: string): MetadataRe
 }
 
 class Extractor {
-    v3String: string = "RPA-3.0";
-    logMessage: logLevelFunction;
-
     private workerPool: WorkerPool;
 
     constructor(logMessage: logLevelFunction) {
-        this.logMessage = logMessage;
         this.workerPool = WorkerPool.getInstance();
     }
 
-    isV1(fileName: string): boolean {
-        return fileName.slice(fileName.lastIndexOf('.') + 1).toLowerCase() === 'rpi'
-    }
-
-    isV2(length: number): boolean {
-        return length === 2
-    }
-
-    isV3(v: string): boolean {
-        return v === this.v3String
-    }
-
-    async parseHeader(filename: string, headerString: string): Promise<RPAHeader> {
-
-        try {
-            if (this.isV1(filename)) {
-                console.log("The RPA-1.0 which has extension '.rpi' is not supported")
-                return failedRPAHeader("Is it RPA file? The archive has errors")
-            }
-
-            const firstLineEndIndex = headerString.indexOf('\n');
-            if (firstLineEndIndex === -1) {
-                console.log("Is it RPA format? The first 100 bytes do not contain a newline character.");
-                return failedRPAHeader("Is it RPA file? The archive has errors")
-            }
-
-            const firstLine = headerString.substring(0, firstLineEndIndex >= 0 ? firstLineEndIndex : headerString.length);
-            const lines = firstLine.split('\n');
-            const header = lines[0];
-            const parts = header.split(' ');
-
-            if (parts.length < 2 || parts.length > 4) {
-                console.log("Is it RPA file? Number of header components doesn't match to any format")
-                return failedRPAHeader("Is it RPA file? The archive has errors")
-            }
-
-            if (this.isV2(parts.length)) {
-                logError("Looks like it's RPA-2.0 format which is not supported");
-                return failedRPAHeader("Is it RPA file? The archive has errors")
-            }
-            let offsetParse = this.stringToBigInt(parts[1]);
-            let keyParse = this.stringToBigInt(parts[2]);
-            if (!offsetParse[1] || !keyParse[1]) {
-                console.log("Is it RPA file? The archive header has errors");
-                return failedRPAHeader("Is it RPA file? The archive has errors")
-            }
-
-            const offsetNumber = offsetParse[0];
-            const keyNumber = keyParse[0];
-
-            if (!this.isV3(parts[0])) {
-                logError("Is it RPA file? It doesn't match to any RPA version");
-                return failedRPAHeader("Is it RPA file? The archive has errors")
-            } else {
-                this.logMessage("Detected version " + this.v3String, LogLevel.Info)
-            }
-
-            return newRPAHeader(Number(offsetNumber), Number(keyNumber), "")
-
-        } catch (error) {
-            console.log("Error processing file " + error);
-        }
-        return failedRPAHeader("Is it RPA file? The archive has errors")
-    }
-
-    async parseMetadata(metadataSrc: Blob, keyNumber: number): Promise<MetadataResponse> {
-        try {
-            // Wrap FileReader operation in a Promise
-            const metadataString: string = await new Promise((resolve, reject) => {
-                const reader = new FileReader();
-                reader.onload = async (e: any): Promise<void> => {
-                    try {
-                        this.workerPool.addTask(new Uint8Array(e.target.result), keyNumber)
-                            .then(content => {
-                                if (content.status === 'finished') {
-                                    resolve(content.content);
-                                }
-                            })
-                            .catch(err => {
-                                reject(err);
-                            });
-                    } catch (err) {
-                        reject(err);
-                    }
-                };
-                reader.readAsArrayBuffer(metadataSrc);
-            });
-            return JSON.parse(metadataString)
-
-        } catch (error) {
-            console.log("Error processing file " + error);
-            return {
-                FileHeaders: [],
-                Error: "Is it RPA file? The archive has errors"
-            }
-        }
-    }
-
     async extractMetadata(file: File, callback: () => void): Promise<MetadataResponse> {
-        const chunkSize = 100; // we assume that RPA header must fit in 100 bytes
-        const headerBlob: Blob = file.slice(0, chunkSize);
-
-        let rpaHead: RPAHeader = await this.parseHeader(file.name, await headerBlob.text());
-        if (rpaHead.Error !== "") {
-            return newMetadataResponse([], rpaHead.Error);
-        }
-        const metadataBlob: Blob = file.slice(rpaHead.offsetNumber);
-        return this.parseMetadata(metadataBlob, rpaHead.keyNumber);
-
-    }
-
-    isHexString(str: string) {
-        const regex1 = /^0x[0-9a-fA-F]+$/i;
-        const regex2 = /^[0-9a-fA-F]+$/i;
-        return regex1.test(str) || regex2.test(str);
-    }
-
-    stringToBigInt(hexString: string): [number, boolean] {
-        if (!this.isHexString(hexString)) {
-            console.log(`"${hexString}" is not hex string`);
-            return [0, false]
-        }
-
-        if (hexString.length % 2 !== 0) {
-            console.error("Hex string must have an even length");
-            return [0, false]
-        }
-
-        const n: number = Number(BigInt("0x" + hexString));
-
-        return [n, true];
+        return this.workerPool.addTask(file)
     }
 }
 
@@ -257,10 +124,10 @@ export class FileSystemAccessApi extends Extractor implements FileSystemAccessAp
 
     async extractMetadata(file: File, callback: () => void): Promise<MetadataResponse> {
         this.file = file
-        let metadata: MetadataResponse = await super.extractMetadata(file, () => {});
+        let metadata: MetadataResponse = await super.extractMetadata(file, () => {
+        });
         if (metadata.Error === "") {
             this.Metadata = metadata.FileHeaders
-            console.log(this.Metadata)
             callback()
         }
         return metadata
@@ -336,7 +203,8 @@ export class FileApi extends Extractor implements FClassInterface {
         this.file = file
 
         this.logMessage(`Analyze "${file.name}"`, LogLevel.Info);
-        let metadata: MetadataResponse = await super.extractMetadata(file, () => {})
+        let metadata: MetadataResponse = await super.extractMetadata(file, () => {
+        })
         if (metadata.Error === "") {
             this.Metadata = metadata.FileHeaders
         }
@@ -549,7 +417,6 @@ export function fileExtractionCreator(firefox: boolean, logMessage: logLevelFunc
         if (firefox) {
             fs = new FileApi(logMessage)
         }
-        console.log(firefox, "LOL", fs)
         return {
             Fs: fs,
             FileName: file.name,
@@ -572,7 +439,6 @@ export async function* scanDir(iterateDirectory: () => AsyncGenerator<File, void
                 onFileSelected(fs)
 
             };
-
 
 
             let metadataPromise: Promise<MetadataResponse> = fs.Fs.extractMetadata(file, callback);
--- src/workers/metadataParser.worker.ts
+++ src/workers/metadataParser.worker.ts
@@ -1,38 +1,8 @@
 import * as workerpool from 'workerpool';
 
-declare function importScripts(...urls: string[]): void;
+import {extractMetadata as em} from "../unrpaLib/unrpaLib";
 
-importScripts("wasm_exec.js");
-
-declare var receiveBytes: (input: Uint8Array, key: number) => string;
-let wasmModule: any;
-const go: Go = new Go();
-
-async function initWasm(): Promise<void> {
-    return new Promise(async (resolve, reject) => {
-        try {
-            const resp = await fetch('unrpa.wasm');
-            const wasm = await WebAssembly.instantiateStreaming(resp, go.importObject);
-            wasmModule = wasm.instance;
-            go.run(wasmModule);
-            resolve();
-        } catch (err) {
-            console.log(err);
-            reject(err);
-        }
-    });
-}
-
-let wasmInition: Promise<void> = initWasm();
-
-async function addTask(data: Uint8Array, keyNumber: number): Promise<any> {
-    await wasmInition;
-    const result = receiveBytes(data, keyNumber);
-    return {status: 'finished', content: result};
-}
-
-// expose the function through workerpool
 workerpool.worker({
-    addTask: addTask
+    extractMetadata: em
 });
 export {};
