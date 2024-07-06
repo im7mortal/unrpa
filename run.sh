72c7c6cffc05fe0b6827dc2d735368e1f6ca05f2
im7mortal <5336231+im7mortal@users.noreply.github.com>
2024-07-05 20:37:31 -0400

    ensure that run.sh will kill dangling container

--- run.sh
+++ run.sh
@@ -1,2 +1,11 @@
- docker build -t unrpa:latest . && \
-docker run --rm --init  -v "$(pwd)":/app -p 3000:3000 --name unrpa_dev unrpa:latest
+#!/bin/bash
+
+# Check if the container is running
+if [ "$(docker ps -q -f name=unrpa_dev)" ]; then
+    echo "Stopping and removing existing unrpa_dev container..."
+    docker kill unrpa_dev
+fi
+
+# Build the new image and run the container
+docker build -t unrpa:latest . && \
+docker run --rm --init -v "$(pwd)":/app -p 3000:3000 --name unrpa_dev unrpa:latest
