580b1b0d1a74bf863cf12737af6134004dcf7b8f
im7mortal <5336231+im7mortal@users.noreply.github.com>
2024-07-07 12:52:53 -0400

    update run.sh; now it waits till container is completely removed

--- run.sh
+++ run.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
-# Check if the container is running
-if [ "$(docker ps -q -f name=unrpa_dev)" ]; then
+# Function to check if the container exists (running or stopped)
+is_container_existing() {
+    [ "$(docker ps -a -q -f name=unrpa_dev)" ]
+}
+
+# Check if the container exists
+if is_container_existing; then
     echo "Stopping and removing existing unrpa_dev container..."
     docker kill unrpa_dev
+
+    # Wait for the container to be removed
+    while is_container_existing; do
+        sleep 1
+    done
 fi
 
 # Build the new image and run the container
