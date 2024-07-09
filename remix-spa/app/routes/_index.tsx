ea414857d277b878131f69a0bce0b5b1ff59488d
im7mortal <5336231+im7mortal@users.noreply.github.com>
2024-07-09 13:04:52 -0400

    migrate public

--- remix-spa/app/routes/_index.tsx
+++ remix-spa/app/routes/_index.tsx
@@ -1,9 +1,24 @@
-import type { MetaFunction } from "@remix-run/node";
+import type {LinksFunction, MetaFunction} from "@remix-run/node";
+
+export const links: LinksFunction = () => {
+  return [
+    { rel: "icon", href: "/favicon.ico" },
+    { rel: "apple-touch-icon", href: "/navbar-logo.png" },
+    { rel: "manifest", href: "/manifest.json" },
+  ];
+};
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix SPA" },
-    { name: "description", content: "Welcome to Remix (SPA Mode)!" },
+    { title: "Renpy archives extractor" },
+    { name: "description", content: "Hassle free RPA extractor. In-Browser, no code, no exes." },
+    { property: "og:site_name", content: "Unrpa" },
+    { property: "og:title", content: "Renpy archives extractor" },
+    { property: "og:description", content: "Hassle free RPA extractor. In-Browser, no code, no exes." },
+    { property: "og:image", content: "/preview.png" },
+    { property: "og:url", content: "/" },
+    { property: "og:type", content: "website" },
+    { name: "theme-color", content: "#000000" },
   ];
 };
 
