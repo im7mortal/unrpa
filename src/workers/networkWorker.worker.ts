59d12b5dac4d0a9d4e994cdda6e2dc3ad0f6127e
im7mortal <5336231+im7mortal@users.noreply.github.com>
2024-07-11 11:00:27 -0400

    DEBUG

--- src/workers/networkWorker.worker.ts
+++ src/workers/networkWorker.worker.ts
@@ -4,7 +4,29 @@ export {};
 
 // Use the correct global scope for Service Workers
 declare const self: ServiceWorkerGlobalScope;
+self.addEventListener('install', function(event) {
+    console.log('Service Worker installing.');
+});
 
+// Activate event
+self.addEventListener('activate', function(event) {
+    console.log('Service Worker activating. !');
+
+    // event.waitUntil(
+    //     caches.keys().then(function(cacheNames) {
+    //         return Promise.all(
+    //             cacheNames.map(function(cacheName) {
+    //                 if (cacheName !== 'my-cache') {
+    //                     console.log('Deleting old cache:', cacheName);
+    //                     return caches.delete(cacheName);
+    //                 }
+    //             })
+    //         );
+    //     }).then(function() {
+    //         console.log('Service Worker is now active.');
+    //     })
+    // );
+});
 
 // Store for the files and groups
 let filesAndGroups = new Map<string, { file: File; group: GroupZipSort[] }>();
@@ -43,7 +65,15 @@ self.addEventListener("fetch", (event: FetchEvent) => {
     console.log("fetch event", event);
     let url = new URL(event.request.url);
 
+    if (url.pathname.startsWith("unrpa/static/js/lol")) {
+        console.log("GATOS");
+        event.respondWith(new Response("lol efe"));
+    }
+
     if (url.pathname.startsWith("/unrpa/zip/")) {
+        console.log("MIU<MIU");
+        return;
+
         const id = url.pathname.split("/unrpa/zip/")[1];
         if (filesAndGroups.has(id)) {
             const {file, group} = filesAndGroups.get(id)!;
@@ -61,4 +91,3 @@ self.addEventListener("fetch", (event: FetchEvent) => {
         }
     }
 });
-
