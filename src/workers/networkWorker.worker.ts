ad45213ff984e7d909cbf8a8d5b8377278841a83
im7mortal <5336231+im7mortal@users.noreply.github.com>
2024-07-11 14:45:10 -0400

    REVERT all dev commits

--- src/workers/networkWorker.worker.ts
+++ src/workers/networkWorker.worker.ts
@@ -1,36 +1,31 @@
 import {ZipWriter} from "@zip.js/zip.js";
-import {FileHeader} from "../unrpaLib/unrpaLibTypes";
-
+import {GroupZipSort} from "../unrpaLib/unrpaGroupFunction";
 export {};
 
 // Use the correct global scope for Service Workers
 declare const self: ServiceWorkerGlobalScope;
-self.addEventListener('install', function (event) {
-    self.skipWaiting();
-});
 
-// Activate event
-self.addEventListener('activate', function (event) {
-    event.waitUntil(self.clients.claim());
-});
 
 // Store for the files and groups
-let filesAndGroups = new Map<string, { file: File; group: FileHeader[] }>();
+let filesAndGroups = new Map<string, { file: File; group: GroupZipSort[] }>();
 
 function readBlobFromFileD(file: File, offset: number, length: number): Blob {
     return file.slice(offset, offset + length);
 }
 
-function createZipStream(file: File, group: FileHeader[]): ReadableStream<Uint8Array> {
+function createZipStream(file: File, group: GroupZipSort[]): ReadableStream<Uint8Array> {
     const {readable, writable} = new TransformStream<Uint8Array>();
-    console.log("STARTED", file)
+
     // Initialize ZipWriter
     const zipWriter = new ZipWriter(writable);
 
     (async () => {
-        for (let f of group) {
-            let blob: Blob = readBlobFromFileD(file, f.Offset, f.Len);
-            await zipWriter.add(f.Name, blob.stream());
+        for (let entry of group) {
+            for (let f of entry.entries) {
+                let blob: Blob = readBlobFromFileD(file, f.Offset, f.Len);
+                // Add each file entry to the ZIP
+                await zipWriter.add(f.Name, blob.stream());
+            }
         }
         // Close the ZipWriter
         await zipWriter.close();
@@ -40,7 +35,7 @@ function createZipStream(file: File, group: FileHeader[]): ReadableStream<Uint8A
 }
 
 self.addEventListener('message', (event: ExtendableMessageEvent) => {
-    const {file, group, id} = event.data as { file: File; group: FileHeader[]; id: string };
+    const {file, group, id} = event.data as { file: File; group: GroupZipSort[]; id: string };
     filesAndGroups.set(id, {file, group});
 });
 
@@ -48,17 +43,11 @@ self.addEventListener("fetch", (event: FetchEvent) => {
     console.log("fetch event", event);
     let url = new URL(event.request.url);
 
-    if (url.pathname.includes("ping")) {
-        console.log("pong");
-        event.respondWith(new Response("pong"));
-    }
-
-    if (url.pathname.includes("zip")) {
+    if (url.pathname.startsWith("/unrpa/zip/")) {
         const id = url.pathname.split("/unrpa/zip/")[1];
-        console.log(`got zip request with id ${id} which ${filesAndGroups.has(id) ? 'exist' : 'does not exist'} in the storage`);
         if (filesAndGroups.has(id)) {
             const {file, group} = filesAndGroups.get(id)!;
-            console.log(id, {file, group})
+
             const zipStream = createZipStream(file, group);
 
             event.respondWith(new Response(zipStream, {
@@ -72,3 +61,4 @@ self.addEventListener("fetch", (event: FetchEvent) => {
         }
     }
 });
+
