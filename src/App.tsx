284bcc9bbb6e90a36081a12524cd83dc097a1b4c
im7mortal <5336231+im7mortal@users.noreply.github.com>
2024-07-11 19:27:42 -0400

    always route to the project path as / can cause problems with service worker

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,39 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import GitHubButton from 'react-github-btn'
+import GitHubButton from 'react-github-btn';
 import UnrpaApp1 from "./UnrpaApp1";
 
 function App() {
     return (
-        <>
-            <div className="row mr-2 mt-2">
-                <div className="col-10 text-left">
-                    ITWILLBEREPLACEDWITHVERSION
+        <Router>
+            <>
+                <div className="row mr-2 mt-2">
+                    <div className="col-10 text-left">
+                        ITWILLBEREPLACEDWITHVERSION
+                    </div>
+
+                    <div className="col-2 text-right">
+                        <GitHubButton href="https://github.com/im7mortal/unrpa"
+                                      data-color-scheme="no-preference: light; light: light; dark: dark;"
+                                      data-icon="octicon-star" data-size="large"
+                                      aria-label="Star buttons/github-buttons on GitHub">Star</GitHubButton>
+                        <GitHubButton href="https://github.com/im7mortal/unrpa/issues"
+                                      data-color-scheme="no-preference: light; light: light; dark: dark;"
+                                      data-icon="octicon-issue-opened" data-size="large"
+                                      aria-label="Issue buttons/github-buttons on GitHub">Issue</GitHubButton>
+                    </div>
                 </div>
 
-                <div className="col-2 text-right">
-                    <GitHubButton href="https://github.com/im7mortal/unrpa"
-                                  data-color-scheme="no-preference: light; light: light; dark: dark;"
-                                  data-icon="octicon-star" data-size="large"
-                                  aria-label="Star buttons/github-buttons on GitHub">Star</GitHubButton>
-                    <GitHubButton href="https://github.com/im7mortal/unrpa/issues"
-                                  data-color-scheme="no-preference: light; light: light; dark: dark;"
-                                  data-icon="octicon-issue-opened" data-size="large"
-                                  aria-label="Issue buttons/github-buttons on GitHub">Issue</GitHubButton>
-                </div>
-            </div>
+                <h1 className="display-1 text-center">UNRPA - Extract Ren'Py Archives</h1>
 
-            <h1 className="display-1 text-center">UNRPA - Extract Ren'Py Archives</h1>
-            <UnrpaApp1/>
-
-        </>
+                <Routes>
+                    <Route path="/unrpa" element={<UnrpaApp1 />} />
+                    <Route path="*" element={<Navigate to="/unrpa" />} />
+                </Routes>
+            </>
+        </Router>
     );
 }
 
