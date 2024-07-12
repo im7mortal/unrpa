4895b77fb12672837206b4e1b94bed1f25d080a3
im7mortal <5336231+im7mortal@users.noreply.github.com>
2024-07-12 12:18:18 -0400

    reformat service worker context

--- src/ContextServiceWorker.tsx
+++ src/ContextServiceWorker.tsx
@@ -27,73 +27,45 @@ export const ServiceWorkerProvider: React.FC<ServiceWorkerProviderProps> = ({chi
     useEffect(() => {
         const registerServiceWorker = async () => {
             try {
-                console.log('registerServiceWorker');
                 if ('serviceWorker' in navigator) {
-                    // Initialize Workbox
                     const wb = new Workbox('/unrpa/service-worker.js');
 
-                    // Event listener for when a new service worker is installed
                     wb.addEventListener('installed', (event) => {
                         if (event.isUpdate) {
-                            console.log('New content is available; please refresh.');
-                            if (confirm("New version available. Do you want to update?")) {
-                                window.location.reload();
-                            }
+                            window.location.reload();
                         } else {
                             console.log('Content is cached for offline use.');
                         }
                     });
 
-                    // Event listener for when the service worker is activated
                     wb.addEventListener('activated', (event) => {
                         console.log('Service Worker activated:', event);
                         setServiceWorker(navigator.serviceWorker.controller);
                     });
 
-                    // Event listener for when a new service worker is waiting to activate
                     wb.addEventListener('waiting', () => {
-                        console.log('A new service worker is waiting to activate.');
-                        if (confirm("New version available. Do you want to update?")) {
-                            wb.messageSkipWaiting();
-                        }
+                        wb.messageSkipWaiting();
                     });
 
-                    // Event listener for when the service worker controlling the page changes
                     navigator.serviceWorker.addEventListener('controllerchange', () => {
-                        console.log('Controller changed');
                         if (navigator.serviceWorker.controller) {
                             setServiceWorker(navigator.serviceWorker.controller);
                             window.location.reload();
                         }
                     });
 
-                    // Register the service worker
                     await wb.register();
-                    console.log('Service Worker registered with Workbox');
+                    const registration = await navigator.serviceWorker.ready;
 
-                    // Wait for the service worker to be ready
-                    const readyRegistration = await navigator.serviceWorker.ready;
-                    console.log('Service Worker ready state:', readyRegistration);
-
-                    // Check if the active service worker is available
-                    if (readyRegistration.active === null) {
-                        console.error('Service Worker active is null. Waiting for it to become active...');
-
-                        // Listen for the 'controllerchange' event to detect when the service worker becomes active
+                    if (registration.active) {
+                        setServiceWorker(registration.active);
+                    } else {
                         navigator.serviceWorker.addEventListener('controllerchange', () => {
                             if (navigator.serviceWorker.controller) {
-                                console.log('Service Worker has become active:', navigator.serviceWorker.controller.state);
                                 setServiceWorker(navigator.serviceWorker.controller);
                             }
                         });
-
-                        // Exit early since the service worker is not active yet
-                        return;
                     }
-
-                    // If the service worker is already active, log its state and set it
-                    console.log('Service Worker is active:', readyRegistration.active.state);
-                    setServiceWorker(readyRegistration.active);
                 } else {
                     console.error('Service Workers are not supported in this browser.');
                 }
@@ -101,6 +73,7 @@ export const ServiceWorkerProvider: React.FC<ServiceWorkerProviderProps> = ({chi
                 console.error('Service Worker registration or activation failed:', error);
             }
         };
+
         if (fileApiWithServiceWorker) {
             registerServiceWorker();
         }
@@ -112,7 +85,6 @@ export const ServiceWorkerProvider: React.FC<ServiceWorkerProviderProps> = ({chi
                 sendMessage({id: "ping"});
             }, 5000);
 
-            // Cleanup interval on unmount
             return () => {
                 clearInterval(pingInterval);
             };
