b61f0c2213016908d0211cb0a201c6f17cdc2916
im7mortal <5336231+im7mortal@users.noreply.github.com>
2024-07-26 22:42:00 -0400

    adding many languages

--- src/LanguageSwitcher.tsx
+++ src/LanguageSwitcher.tsx
@@ -18,8 +18,20 @@ const LanguageSwitcher: React.FC = () => {
 
             <Dropdown.Menu>
                 <Dropdown.Item onClick={() => changeLanguage('en')}>🍔 English</Dropdown.Item>
-                <Dropdown.Item onClick={() => changeLanguage('es')}>🌮 Español</Dropdown.Item>
+                <Dropdown.Item onClick={() => changeLanguage('es')}>🧉 Español</Dropdown.Item>
                 <Dropdown.Item onClick={() => changeLanguage('ru')}>🥟 Русский</Dropdown.Item>
+                <Dropdown.Item onClick={() => changeLanguage('zh')}>🥢 中文</Dropdown.Item>
+                <Dropdown.Item onClick={() => changeLanguage('pt-BR')}>💃 Português</Dropdown.Item>
+                <Dropdown.Item onClick={() => changeLanguage('ar')}>🌡 العربية</Dropdown.Item>
+                <Dropdown.Item onClick={() => changeLanguage('hi')}>🍶 हिंदी</Dropdown.Item>
+                <Dropdown.Item onClick={() => changeLanguage('fr')}>🍾 Français</Dropdown.Item>
+                <Dropdown.Item onClick={() => changeLanguage('bn')}>🥦 বাংলা</Dropdown.Item>
+                <Dropdown.Item onClick={() => changeLanguage('ja')}>🍣 日本語</Dropdown.Item>
+                <Dropdown.Item onClick={() => changeLanguage('pa')}>🥕 ਪੰਜਾਬੀ</Dropdown.Item>
+                <Dropdown.Item onClick={() => changeLanguage('de')}>🍺 Deutsch</Dropdown.Item>
+                <Dropdown.Item onClick={() => changeLanguage('jv')}>🌶 Jawa</Dropdown.Item>
+                <Dropdown.Item onClick={() => changeLanguage('ms')}>🥠 Melayu</Dropdown.Item>
+                <Dropdown.Item onClick={() => changeLanguage('tr')}>🥘 Türkçe</Dropdown.Item>
             </Dropdown.Menu>
         </Dropdown>
     );
--- src/i18n.ts
+++ src/i18n.ts
@@ -4,17 +4,41 @@ import LanguageDetector from 'i18next-browser-languagedetector';
 
 // Import translation files
 import en from './locales/en.json';
+import zh from './locales/zh.json';
 import es from './locales/es.json';
+import ar from './locales/ar.json';
+import pt from './locales/pt.json';
 import ru from './locales/ru.json';
+import hi from './locales/hi.json';
+import fr from './locales/fr.json';
+import bn from './locales/bn.json';
+import ja from './locales/ja.json';
+import pa from './locales/pa.json';
+import de from './locales/de.json';
+import jv from './locales/jv.json';
+import ms from './locales/ms.json';
+import tr from './locales/tr.json';
 
 i18n
     .use(LanguageDetector) // Detect user language
     .use(initReactI18next) // Passes i18n down to react-i18next
     .init({
         resources: {
-            en: {translation: en},
-            es: {translation: es},
-            ru: {translation: ru},
+            en: { translation: en },
+            zh: { translation: zh },
+            hi: { translation: hi },
+            es: { translation: es },
+            fr: { translation: fr },
+            ar: { translation: ar },
+            bn: { translation: bn },
+            pt: { translation: pt },
+            ru: { translation: ru },
+            ja: { translation: ja },
+            pa: { translation: pa },
+            de: { translation: de },
+            jv: { translation: jv },
+            ms: { translation: ms },
+            tr: { translation: tr },
         },
         fallbackLng: {
             'default': ['en'],
