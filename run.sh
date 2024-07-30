640b6a48f54d08042a9a7df8586d186796a1e78d
im7mortal <5336231+im7mortal@users.noreply.github.com>
2024-07-30 10:51:37 -0400

    make image and container names variables

--- run.sh
+++ run.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
+CONTAINER_NAME="unrpa_dev"
+IMAGE_NAME="unrpa:latest"
+
 # Function to check if the container exists (running or stopped)
 is_container_existing() {
-    [ "$(docker ps -a -q -f name=unrpa_dev)" ]
+    [ "$(docker ps -a -q -f name=$CONTAINER_NAME)" ]
 }
 
 # Check if the container exists
 if is_container_existing; then
-    echo "Stopping and removing existing unrpa_dev container..."
-    docker kill unrpa_dev
+    echo "Stopping and removing existing $CONTAINER_NAME container..."
+    docker kill $CONTAINER_NAME
 
     # Wait for the container to be removed
     while is_container_existing; do
@@ -17,5 +20,5 @@ if is_container_existing; then
 fi
 
 # Build the new image and run the container
-docker build -t unrpa:latest . && \
-docker run --rm --init -v "$(pwd)":/app -p 5173:5173 --name unrpa_dev unrpa:latest
+docker build -t $IMAGE_NAME . && \
+docker run --rm --init -v "$(pwd)":/app -p 5173:5173 --name $CONTAINER_NAME $IMAGE_NAME
