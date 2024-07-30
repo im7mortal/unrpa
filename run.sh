c056e9c3cdf7bf31d4d679894fe31f44d3047461
im7mortal <5336231+im7mortal@users.noreply.github.com>
2024-07-30 10:42:00 -0400

    make docker set up work

--- run.sh
+++ run.sh
@@ -18,4 +18,4 @@ fi
 
 # Build the new image and run the container
 docker build -t unrpa:latest . && \
-docker run --rm --init -v "$(pwd)":/app -p 3000:3000 --name unrpa_dev unrpa:latest
+docker run --rm --init -v "$(pwd)":/app -p 5173:5173 --name unrpa_dev unrpa:latest
