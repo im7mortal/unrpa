203e491907299f36b0fbb78444b9140455c31379
im7mortal <5336231+im7mortal@users.noreply.github.com>
2024-07-30 20:57:24 -0400

    new preview layout generator

--- tools/screenshotmaker/screenshot.py
+++ tools/screenshotmaker/screenshot.py
@@ -4,6 +4,7 @@ from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from PIL import Image, ImageDraw
 import time
 
 def get_webdriver(browser):
@@ -26,7 +27,7 @@ def get_webdriver(browser):
 default_width = 1000
 # language drop down is very big; if it's go out of screen it will fail
 language_pick_height = 1200
-screenshot_height = 900
+screenshot_height = 600
 
 # Initialize WebDriver
 driver = get_webdriver('firefox')
@@ -43,6 +44,25 @@ languages = ['hi', 'bn', 'te', 'mr', 'gu', 'pa', 'ta', 'th', 'ur', 'fa', 'ar', '
 
 # Create a WebDriverWait object
 wait = WebDriverWait(driver, 10)
+
+header_img = "template/header.png"
+header = Image.open(header_img)
+
+def add_header_to_screenshot(screenshot_path, header_path, output_path):
+    screenshot = Image.open(screenshot_path)
+
+    width = screenshot.width
+    height = screenshot.height + header.height + 210  # 210 px empty white space
+
+    new_image = Image.new('RGB', (width, height), (255, 255, 255))  # Create new white image
+
+    # Paste the header and screenshot on the new image
+    new_image.paste(header, (0, 210))
+    new_image.paste(screenshot, (0, header.height + 210))
+
+    new_image.save(output_path)
+    print(f'Screenshot saved as {output_path}')
+
 
 # Loop through each language, switch, and take a screenshot
 for lang in languages:
@@ -61,7 +81,9 @@ for lang in languages:
     driver.set_window_size(default_width, screenshot_height)  # Default height can be adjusted as needed
     screenshot_name = f'preview_{lang}.png'
     driver.save_screenshot(screenshot_name)
-    print(f'Screenshot saved as {screenshot_name}')
+
+    # Add header to the screenshot
+    add_header_to_screenshot(screenshot_name, header_img, screenshot_name)
 
     # Restore increased height after taking the screenshot
     driver.set_window_size(default_width, language_pick_height)  # Adjust the height as needed
