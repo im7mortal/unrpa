effed7a3d0a5b0c7dc2c29902fa3b9f0fa923d80
im7mortal <5336231+im7mortal@users.noreply.github.com>
2026-04-11 22:26:27 -0400

    #7 improve CI hygiene

--- src/ContextServiceWorker.tsx
+++ src/ContextServiceWorker.tsx
@@ -1,4 +1,5 @@
-import React, {createContext, useContext, useEffect, useState, ReactNode} from 'react';
+import React, {createContext, useContext, useEffect, useState} from 'react';
+import type {ReactNode} from 'react';
 import {Workbox} from 'workbox-window';
 import ApiInfoContext from "./ContextAPI";
 
--- src/ElementGithubButtons.tsx
+++ src/ElementGithubButtons.tsx
@@ -1,4 +1,4 @@
-import React, {FC, MouseEvent, useContext} from 'react';
+import React from 'react';
 
 import {useTranslation} from "react-i18next";
 import GitHubButton from "react-github-btn";
